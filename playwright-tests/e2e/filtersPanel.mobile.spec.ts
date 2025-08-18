d408ece92167a1b5691aaa4d30c14f3bc4e2fd16
Muratov Egor <egor_muratov@hotmail.com>
2025-08-18 11:36:33 +0200

    New tests have been added for the filter panel: checking active and inactive filter states, as well as tests for maximum depth and minimum visibility sliders. Methods for waiting for slider values and filter states have been updated in the FiltersPanel class.

--- playwright-tests/e2e/filtersPanel.mobile.spec.ts
+++ playwright-tests/e2e/filtersPanel.mobile.spec.ts
@@ -40,6 +40,7 @@ test.describe('Mobile: Filters Panel', () => {
     await filtersPanel.chooseSiteTypeFilterById(2);
 
     await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+    await filtersPanel.expectSiteTypeFilterActive(2);
     await filtersPanel.closeMobileFiltersPanel();
     await markers.assertThatClustersCountLessThan(initClusters);
   });
@@ -53,6 +54,8 @@ test.describe('Mobile: Filters Panel', () => {
     await filtersPanel.chooseDifficultyFilterById(2);
 
     await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+    await filtersPanel.expectDifficultyFilterActive(2);
+    await filtersPanel.closeMobileFiltersPanel();
     await markers.assertThatClustersCountLessThan(initClusters);
   });
 
@@ -65,6 +68,90 @@ test.describe('Mobile: Filters Panel', () => {
     await filtersPanel.chooseRatingById(4);
 
     await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+    await filtersPanel.expectRatingFilterActive(4);
+    await filtersPanel.closeMobileFiltersPanel();
+    await markers.assertThatClustersCountLessThan(initClusters);
+  });
+
+  test('Toggle dive site type filter on/off', async ({ page }) => {
+    const filtersPanel = new FiltersPanel(page);
+    const markers = new MarkersPage(page);
+
+    const initClusters = await markers.getClustersCount();
+    await filtersPanel.openMobileFiltersPanel();
+    await filtersPanel.chooseSiteTypeFilterById(1);
+    await filtersPanel.expectSiteTypeFilterActive(1);
+    await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+
+    await filtersPanel.chooseSiteTypeFilterById(1);
+    await filtersPanel.expectSiteTypeFilterNotActive(1);
+    await filtersPanel.expectButtonCleanAllFiltersIsNotExist();
+    await filtersPanel.closeMobileFiltersPanel();
+
+    await markers.assertThatClustersCountIsEqual(initClusters);
+  });
+
+  test('Toggle dive difficulty filter on/off', async ({ page }) => {
+    const filtersPanel = new FiltersPanel(page);
+    const markers = new MarkersPage(page);
+
+    const initClusters = await markers.getClustersCount();
+    await filtersPanel.openMobileFiltersPanel();
+    await filtersPanel.chooseDifficultyFilterById(2);
+    await filtersPanel.expectDifficultyFilterActive(2);
+    await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+
+    await filtersPanel.chooseDifficultyFilterById(2);
+    await filtersPanel.expectDifficultyFilterNotActive(2);
+    await filtersPanel.expectButtonCleanAllFiltersIsNotExist();
+    await filtersPanel.closeMobileFiltersPanel();
+
+    await markers.assertThatClustersCountIsEqual(initClusters);
+  });
+
+  test('Toggle dive rating filter on/off', async ({ page }) => {
+    const filtersPanel = new FiltersPanel(page);
+    const markers = new MarkersPage(page);
+
+    const initClusters = await markers.getClustersCount();
+    await filtersPanel.openMobileFiltersPanel();
+    await filtersPanel.chooseRatingById(4);
+    await filtersPanel.expectRatingFilterActive(4);
+    await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+
+    await filtersPanel.chooseRatingById(4);
+    await filtersPanel.expectRatingFilterNotActive(4);
+    await filtersPanel.expectButtonCleanAllFiltersIsNotExist();
+    await filtersPanel.closeMobileFiltersPanel();
+
+    await markers.assertThatClustersCountIsEqual(initClusters);
+  });
+
+  test('Set max depth slider filter', async ({ page }) => {
+    const filtersPanel = new FiltersPanel(page);
+    const markers = new MarkersPage(page);
+
+    const initClusters = await markers.getClustersCount();
+    await filtersPanel.openMobileFiltersPanel();
+    await filtersPanel.setMaxDepthSlider(10);
+
+    await filtersPanel.expectMaxDepthSliderValue(10);
+    await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+    await filtersPanel.closeMobileFiltersPanel();
+    await markers.assertThatClustersCountLessThan(initClusters);
+  });
+
+  test('Set min visibility slider filter', async ({ page }) => {
+    const filtersPanel = new FiltersPanel(page);
+    const markers = new MarkersPage(page);
+
+    const initClusters = await markers.getClustersCount();
+    await filtersPanel.openMobileFiltersPanel();
+    await filtersPanel.setMinVisibilitySlider(16);
+
+    await filtersPanel.expectMinVisibilitySliderValue(16);
+    await filtersPanel.expectButtonCleanAllFiltersIsVisible();
+    await filtersPanel.closeMobileFiltersPanel();
     await markers.assertThatClustersCountLessThan(initClusters);
   });
 });
