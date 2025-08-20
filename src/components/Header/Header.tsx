362c1f143a65dd8d5a81cc49f0222011e0beb42a
Muratov Egor <egor_muratov@hotmail.com>
2025-08-20 20:20:11 +0200

    Added a new Logo component for the header, which includes the logo and slogan. Updated styles for the Yomogi font. Localizations updated to add the slogan in English and Russian. Updated dependencies.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,15 @@
 'use client';
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
 import NavBar from './NavBar';
+import Logo from './Logo';
 
 export default function Header() {
-  const { t } = useTranslation('common');
-
   return (
-    <header className="sm:py-2 sm:px-10 p-2 flex justify-between items-center border-b border-gray-200 dark:border-gray-700 min-h-[4rem] bg-background transition-colors">
-      <Link href="/" className="flex sm:gap-2 gap-1 items-center hover:underline pr-4 ">
-        <Image
-          src="/img/Logo.ico"
-          alt=""
-          width={64}
-          height={64}
-          className="sm:w-24 sm:h-24 w-16 h-16"
-          data-testid="header-logo"
-        />
-        <h1
-          className="text-xl sm:text-3xl font-bold text-gray-900 dark:text-white"
-          suppressHydrationWarning
-        >
-          {t('appName')}
-        </h1>
-      </Link>
+    <header
+      className="sm:py-2 sm:px-10 p-2 flex justify-between items-center border-b border-gray-200 dark:border-gray-700 min-h-[4rem] bg-background transition-colors"
+      suppressHydrationWarning
+    >
+      <Logo />
       <NavBar />
     </header>
   );
